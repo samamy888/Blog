e68979365282ae375fe422fa95687bfaa0c2aafc
wenhui_xiao <wenhui_xiao@9splay.com>
2020-07-01 16:08:47 +0800

    router修復

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,7 +36,7 @@ module.exports = {
 						children: [
 							{
 								title: 'SQL變數為null問題',
-								path: '/note/SQL/Sql_NullQuestion',
+								path: '/blog/note/SQL/Sql_NullQuestion',
 								content: '之前最怕遇到的就是選擇的參數有時候會帶null' +
 									'，那時候遇到這種的都改用字串寫法，如果變數一多就會變的很恐怖' +
 									'，後來發現有其他寫法，就在也離不開了。',
@@ -56,7 +56,7 @@ module.exports = {
 						children: [
 							{
 								title: '前端推薦網站:https://placeholder.com',
-								path: '/note/HTML/placeholder',
+								path: '/blog/note/HTML/placeholder',
 								content: '前幾天朋友跟我分享這個網站，' +
 									'看完之後發現這也太好用了，' +
 									'寫在blog來跟大家推薦一下😅😅',
@@ -75,14 +75,14 @@ module.exports = {
 						children: [
 							{
 								title: '字數過多問題',
-								path: 'note/CSS/WordCount_Solution',
+								path: '/blognote/CSS/WordCount_Solution',
 								content: '之前遇到字數過多的問題，' +
 									'很笨的我都用正則去解決🤣🤣，' +
 									'直到爬文才發現還有這種寫法',
 							},
 							{
 								title: '置中產生器',
-								path: 'note/CSS/CenterCreate',
+								path: '/blognote/CSS/CenterCreate',
 								content: '有時候會煩惱置中的權重問題，' +
 									'這裡就有無腦的置中產生器 哈哈，',
 							},
@@ -96,14 +96,14 @@ module.exports = {
 				children: [
 					{
 						title: 'Lodash套件',
-						path: '/recommend_kit/Lodash',
+						path: '/blog/recommend_kit/Lodash',
 						content: '這款套件是我最喜歡的套件之一，' +
 							'強大的功能 甚至有些寫法比原生還厲害，' +
 							'這篇主要是紀錄幾個有用的',
 					},
 					{
 						title: 'DataTables套件',
-						path: '/recommend_kit/DataTables',
+						path: '/blog/recommend_kit/DataTables',
 						content: '雖然bootstrap的css已經足夠好看了，' +
 							'但是這款套件除了可以好看，' +
 							'還能做到許多支持' +
@@ -112,13 +112,13 @@ module.exports = {
 					},
 					{
 						title: 'sweetalert2套件',
-						path: '/recommend_kit/sweetalert2',
+						path: '/blog/recommend_kit/sweetalert2',
 						content: '我覺得這款套件的alert框很好看，' +
 							'搭配icon做出的演示更是妙不可言，',
 					},
 					{
 						title: 'Swiper套件',
-						path: '/recommend_kit/Swiper',
+						path: '/blog/recommend_kit/Swiper',
 					},
 				]
 			},
