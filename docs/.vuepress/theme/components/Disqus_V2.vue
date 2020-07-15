6a7027a18c83600edcd4677a87e015925b94b6fc
wenhui_xiao <wenhui_xiao@9splay.com>
2020-07-15 15:04:01 +0800

    改用自己寫的disqus

--- docs/.vuepress/theme/components/Disqus_V2.vue
+++ docs/.vuepress/theme/components/Disqus_V2.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="disqus_border">
-    <Disqus />
+    <Disqus shortname="https-samamy888-github-io-blog" />
   </div>
 </template>
 <style>
@@ -13,3 +13,12 @@
   margin-top: 100px;
 }
 </style>
+<script>
+import { Disqus } from "vue-disqus";
+export default {
+  name: "Disqus_V2",
+  components: {
+    Disqus
+  }
+};
+</script>>
