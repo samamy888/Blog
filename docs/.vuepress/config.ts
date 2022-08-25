32f2b3784a1b78d1c58d69c341c1a2702d21a4fc
wenhui <WenHui_Xiao@everrich.com.tw>
2022-08-25 16:18:55 +0800

    備份一下

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,9 @@
+import { defaultTheme, defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-TW',
+  title: '文暉blog',
+  description: 'Just playing around',
+  theme: defaultTheme({
+  }),
+})
