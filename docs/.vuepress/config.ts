156879b4df7420ad678f828ac85a077ce69adcb7
wenhui <WenHui_Xiao@everrich.com.tw>
2022-08-26 10:10:41 +0800

    feature : sidebar調整

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,9 +1,15 @@
+import { ref } from 'vue'
 import { defaultTheme, defineUserConfig } from 'vuepress'
+import { service } from './service'
 
+const _service = new service()
+const sidebar =  ref<string[]>(await _service.getSidebar())
+console.log(sidebar.value)
 export default defineUserConfig({
   lang: 'zh-TW',
   title: '文暉blog',
   description: 'Just playing around',
   theme: defaultTheme({
+    sidebar: sidebar.value
   }),
 })
