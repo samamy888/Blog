b1ab33d6033db0440a05040da5985bd008da7e05
wenhui <WenHui_Xiao@everrich.com.tw>
2022-08-29 15:35:34 +0800

    feature : Layout

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,15 +1,25 @@
 import { ref } from 'vue'
-import { defaultTheme, defineUserConfig } from 'vuepress'
+import { defineUserConfig } from 'vuepress'
 import { service } from './service'
+import { commentPlugin } from "vuepress-plugin-comment2";
+import { localTheme } from './theme'
 
 const _service = new service()
 const sidebar =  ref<string[]>(await _service.getSidebar())
-console.log(sidebar.value)
 export default defineUserConfig({
   lang: 'zh-TW',
   title: '文暉blog',
+  plugins: [
+    commentPlugin({
+      provider: "Giscus",
+      repo:'samamy888/blog',
+      repoId:'MDEwOlJlcG9zaXRvcnkyNjMyMjA3NzI=',
+      category:'Announcements',
+      categoryId:'DIC_kwDOD7BuJM4CRDlW'
+    }),
+  ],
   description: 'Just playing around',
-  theme: defaultTheme({
+  theme: localTheme({
     sidebar: sidebar.value
   }),
 })
---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,30 @@
+
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+const isDarkMode = ref(false);
+let observer;
+onMounted(() => {
+  const html = document.querySelector("html") as HTMLElement;
+  isDarkMode.value = html.classList.contains("dark");
+  // watch theme change
+  observer = new MutationObserver(() => {
+    isDarkMode.value = html.classList.contains("dark");
+  });
+  observer.observe(html, {
+    attributeFilter: ["class"],
+    attributes: true,
+  });
+});
+onBeforeUnmount(() => {
+  observer.disconnect();
+});
+</script>
+
+<template>
+  <ParentLayout>
+    <template #page-content-bottom>
+      <CommentService :darkmode="isDarkMode" />
+    </template>
+  </ParentLayout>
+</template>
