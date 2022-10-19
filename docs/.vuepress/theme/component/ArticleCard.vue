3921fb2d8a41159b8542f6152fe0a1d86aa018e9
wenhui <WenHui_Xiao@everrich.com.tw>
2022-10-19 11:43:19 +0800

    feature : 選單調整

--- docs/.vuepress/theme/component/ArticleCard.vue
+++ docs/.vuepress/theme/component/ArticleCard.vue
@@ -1,34 +1,56 @@
 <template>
-  <div class="theme-default-content card">
-    <p @click="gotoLink(item.link)">
-      {{ item.text }}
+  <div :class="background" class="theme-default-content card">
+    <p class="content" @click="gotoLink(item.link)">
+      <b>{{ item.text }}</b>
     </p>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref, PropType } from "vue";
+import { onMounted, ref, PropType, watch } from "vue";
 import { sidebarItem } from "../../interface/sidebarItem";
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 const sidebar = ref();
-const router = useRouter()
+const router = useRouter();
 const props = defineProps({
   item: { type: Object as PropType<sidebarItem>, required: true },
-  darkmode: Boolean
+  darkmode: Boolean,
 });
-const { item, darkmode } = props;
+const { item } = props;
+const background = ref();
 onMounted(async () => {
-  console.log(item.text);
-  console.log(darkmode);
+  if (props.darkmode) background.value = "dark-background";
+  else background.value = "white-background";
+  console.log(item)
 });
+watch(
+  () => props.darkmode,
+  (newValue) => {
+    if (newValue) background.value = "dark-background";
+    else background.value = "white-background";
+  }
+);
 const gotoLink = (link) => {
-    router.push(link)
-}
+  router.push(link);
+};
 </script>
 <style scoped>
-    .card {
-        height: 100px;
-        width: 300px;
-        background-color: white;
-        color: black;
-    }
+.card {
+  height: 100px;
+  width: 300px;
+  border-radius: 20px;
+  
+}
+.content {
+  font-size: 1.2rem;
+  padding-top: 20px;
+}
+.dark-background {
+  background-color: white;
+  color: black;
+}
+.white-background {
+  background-color: black;
+  color: white;
+  box-shadow: 0 0 20px 8px #d0d0d0;
+}
 </style>
--- docs/.vuepress/theme/component/ArticleList.vue
+++ docs/.vuepress/theme/component/ArticleList.vue
@@ -1,17 +1,16 @@
 <template >
-    <template v-for="item in sidebar">
-        <ArticleCard :item=item :darkmode=darkmode />
+    <template v-for="item in sidebar" :key="item">
+        <ArticleCard  :item="item" :darkmode="props.darkmode" />
     </template>
 </template>
 <script setup lang="ts">
-import { onMounted,ref  } from "vue";
+import { onMounted,ref,watch  } from "vue";
 import ArticleCard from './ArticleCard.vue'
 import { useSidebarItems } from '@vuepress/theme-default/client';
 const sidebar = ref();
 const props = defineProps({
     darkmode: Boolean
 })
-const { darkmode } = props
 onMounted(async () => {
     sidebar.value = useSidebarItems().value.map(x=>x.children).flatMap(x=>x)
 });
